f28110d3685e7c37fe86acd37ecccbeb719a0ac3
LP269 <aartichaple2124@gmail.com>
2025-06-28 20:46:25 +0530

    Frontend shell

--- cart.sh
+++ cart.sh
@@ -1,12 +1,23 @@
-[Unit]
-Description = Cart Service
-[Service]
-User=roboshop
-Environment=REDIS_HOST=redis.learntechnology.shop
-Environment=CATALOGUE_HOST=catalogue.learntechnology.shop
-Environment=CATALOGUE_PORT=8080
-ExecStart=/bin/node /app/server.js
-SyslogIdentifier=cart
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
 
-[Install]
-WantedBy=multi-user.target
+dnf install nodejs -y
+
+cp cart.service /etc/systemd/system/cart.service
+
+useradd roboshop
+
+rm-rf /app
+mkdir /app
+
+curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip
+cd /app
+unzip /tmp/cart.zip
+
+cd /app
+npm install
+
+systemctl daemon-reload
+
+systemctl enable cart
+systemctl start cart
