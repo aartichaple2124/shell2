572d327b4e1c8c04105571d3808af033ee1dc70f
LP269 <aartichaple2124@gmail.com>
2025-06-28 15:27:50 +0530

    Frontend shell

---
+++ Mongodb.sh
@@ -0,0 +1,10 @@
+dnf install mongodb-org -y
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+systemctl enable mongod
+systemctl start mongod
+
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+
+
+systemctl restart mongod
